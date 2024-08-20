48b7f06df2d7a72f5f132ef38a0c8ac0dad0b7f3
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2024-08-20 23:44:32 +0300

    feat: implement internationalization

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from "next/link"
+import { getDictionary } from '../../lib/dictionary'
+import { Locale } from '../../../i18n.config'
+import LanguageSwitcher from '../languageSwitcher/languageSwitcher.component'
+
+interface HeaderProps {
+  lang: Locale
+}
+
+
+async function Header({lang}:HeaderProps) {
+  const { navigation } = await getDictionary(lang)
+  return (
+    <header>
+      <nav className='flex justify-center gap-4 p-4 bg-green-300'>
+        
+      <Link href={`/${lang}`}>{navigation.home}</Link>
+      <Link href={`/${lang}/about`}>{navigation.about}</Link>
+      <LanguageSwitcher />
+      </nav>
+    </header>
+  )
+}
+
+export default Header

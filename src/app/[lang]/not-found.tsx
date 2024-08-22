5bfc88bccedc256f032309e82c5e3b6e558eb6f1
Aleksandr Kisel <51911663+Alex991995@users.noreply.github.com>
2024-08-22 20:31:06 +0300

    refactor: check commit

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFoundPage(): JSX.Element {
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, this page was not found.</p>
+     
     </div>
   );
 }

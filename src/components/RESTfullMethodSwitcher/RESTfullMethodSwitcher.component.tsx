b8de779c4030261a07a0d07fdbeb4e737978fd28
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-09-03 22:56:41 +0300

    feat: add rest-client.

---
+++ src/components/RESTfullMethodSwitcher/RESTfullMethodSwitcher.component.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState } from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '../ui/select';
+import { SelectGroup } from '@radix-ui/react-select';
+
+export default function MethodSwitcher(props: { onChange: (v: string) => void }): JSX.Element {
+  const Methods: string[] = ['GET', 'PUT', 'POST', 'DELETE'];
+
+  const [selectedMethod, setselectedMethod] = useState(Methods[0]);
+
+  const handleMethodChange = (value: string) => {
+    setselectedMethod(value);
+    props.onChange(value);
+  };
+
+  return (
+    <Select onValueChange={e => handleMethodChange(e as string)} value={selectedMethod}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a method" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Method</SelectLabel>
+          {Methods.map((method, i) => (
+            <SelectItem key={i} value={method}>
+              {method}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}

73607cb0b27d05702c8e0fba06fb5dc73211d501
Yaraslava Adzinets <yaroslava.odin.00@gmail.com>
2024-09-15 20:02:12 +0300

    fix: add suspense.

--- src/app/[lang]/restfull-client/page.tsx
+++ src/app/[lang]/restfull-client/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import JSONViewer from '../../../components/JSONViewer/JSONViewer';
 import RESTfullClient from '../../../components/RESTfullClient/RESTfullClient';
 
 export default function RESTfullPage(): JSX.Element {
   return (
-    <RESTfullClient>
-      <JSONViewer value="" statusCode={undefined} />
-    </RESTfullClient>
+    <Suspense>
+      <RESTfullClient>
+        <JSONViewer value="" statusCode={undefined} />
+      </RESTfullClient>
+    </Suspense>
   );
 }
